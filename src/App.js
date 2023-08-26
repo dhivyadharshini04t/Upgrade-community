8697b79cd66db35c100aca0fcea425da10cf592d
dhivyadharshini04t <125949853+dhivyadharshini04t@users.noreply.github.com>
2023-08-26 21:31:57 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react';
+import './App.css'; // Import your CSS file for styling
 
-function App() {
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increaseCount = () => {
+    setCount(count + 1);
+  };
+
+  const decreaseCount = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
   return (
-    <div>
-      <h1>Hello, World!</h1>
+    <div className="counter-container">
+      <h1 className="counter">{count}</h1>
+      <div className="button-container">
+        <button className="btn" onClick={increaseCount}>Increase</button>
+        <button className="btn" onClick={decreaseCount}>Decrease</button>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Counter;
